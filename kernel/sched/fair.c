2640dfdc97768cc4653946ce131b45b19eb3b804
Joel Fernandes <joelaf@google.com>
2017-07-20 23:46:56 -0700

    sched/fair: Skip frequency updates if CPU about to idle
    If CPU is about to idle, prevent a frequency update. With the number of
    schedutil governor wake ups are reduced by more than half on a test
    playing bluetooth audio.
    
    Test: sugov wake ups drop by more than half when playing music with
    screen off (476 / 1092)
    
    Bug: 64689959
    
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    Change-Id: I5df756a7389268eb08ce7b00017259c995aa8e35
    [Kazuki: Port to v5.4]
    Signed-off-by: Kazuki H <kazukih0205@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    (cherry picked from commit b65782f8c07b5b3e65c7ddcce7d7d5458f84bf38)
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: maxsteeel <quinteroalejandro2009@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3587,6 +3587,7 @@ static void detach_entity_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *s
 #define UPDATE_TG	0x1
 #define SKIP_AGE_LOAD	0x2
 #define DO_ATTACH	0x4
+#define SKIP_CPUFREQ	0x8
 
 /* Update task and its cfs_rq load average */
 static inline void update_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
@@ -3897,6 +3898,7 @@ static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
 #define UPDATE_TG	0x0
 #define SKIP_AGE_LOAD	0x0
 #define DO_ATTACH	0x0
+#define SKIP_CPUFREQ	0x0
 
 static inline void update_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *se, int not_used1)
 {
@@ -4031,6 +4033,7 @@ static inline bool cfs_bandwidth_used(void);
 static void
 enqueue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 {
+	int update_flags;
 	bool renorm = !(flags & ENQUEUE_WAKEUP) || (flags & ENQUEUE_MIGRATED);
 	bool curr = cfs_rq->curr == se;
 
@@ -4060,7 +4063,12 @@ enqueue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 	 *     its group cfs_rq
 	 *   - Add its new weight to cfs_rq->load.weight
 	 */
-	update_load_avg(cfs_rq, se, UPDATE_TG | DO_ATTACH);
+	update_flags = UPDATE_TG | DO_ATTACH;
+
+	if (flags & DEQUEUE_IDLE)
+		update_flags |= SKIP_CPUFREQ;
+
+	update_load_avg(cfs_rq, se, update_flags);
 	update_cfs_group(se);
 	enqueue_runnable_load_avg(cfs_rq, se);
 	account_entity_enqueue(cfs_rq, se);
@@ -5471,10 +5479,13 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		flags |= DEQUEUE_SLEEP;
 	}
 
+	if (task_sleep && rq->nr_running == 1)
+		flags |= DEQUEUE_IDLE;
+
 	for_each_sched_entity(se) {
 		cfs_rq = cfs_rq_of(se);
 
-		update_load_avg(cfs_rq, se, UPDATE_TG);
+		update_load_avg(cfs_rq, se, UPDATE_TG | (flags & DEQUEUE_IDLE));
 		update_cfs_group(se);
 
 		cfs_rq->h_nr_running--;
