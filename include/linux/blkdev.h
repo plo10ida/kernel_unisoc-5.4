153cc62af56351a50c77808ebc28e862c7f4738e
minarypenguin <alexfinhart@gmail.com>
2022-05-31 17:58:45 +0530

    block: reduce BLKDEV_MAX_RQ to 64
    write /sys/block/sda/queue/nr_requests 64
    https://cs.android.com/android/platform/superproject/+/master:device/google/gs101/conf/init.gs101.rc;l=721;drc=4dc5683053c41895fcc37d988937b859d130801d
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I9da82bf672b019e216a1f996038ef11b3bcb9434
    Signed-off-by: maxsteeel <quinteroalejandro2009@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -47,7 +47,7 @@ struct blk_stat_callback;
 struct blk_keyslot_manager;
 
 #define BLKDEV_MIN_RQ	4
-#define BLKDEV_MAX_RQ	128	/* Default maximum */
+#define BLKDEV_MAX_RQ	64	/* Default maximum */
 
 /* Must be consistent with blk_mq_poll_stats_bkt() */
 #define BLK_MQ_POLL_STATS_BKTS 16
